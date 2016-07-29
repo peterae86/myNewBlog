8ec8d0c5582205c8dace5fa267c6d1476509628b
xiaorui.guo <xiaorui.guo@jimu.com>
2016-07-29 18:58:57 +0800

    try

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http2 = require("http2");
+var http2 = require("spdy");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -11,8 +11,9 @@ var users = require('./routes/users');
 var fs = require("fs");
 var http = require("http");
 var url = require("url");
-var log = require("log4js");
+var net = require("net");
 var app = express();
+var proxy = require("spdyproxy")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,131 +60,110 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-var fun = function (a) {
-    var log2 = log.getLogger(a.component)
-    log2.child = fun;
-    return log2
-};
-log.child = fun;
 
 http2.createServer({
-             log: log,
-        key: fs.readFileSync('./my.key'),
-        cert: fs.readFileSync('./my.crt')
-    },
-
-    function (req, resp) {
-        if (req.method == 'CONNECT') {
-            resp.socket.write("HTTP/2 200 Connection established\nProxy-Agent: THE BB Proxy\n\n");
-            return;
+    //   log: log,
+    key: fs.readFileSync('./my.key'),
+    cert: fs.readFileSync('./my.crt')
+}).on('request', function (req, resp) {
+    console.log(222)
+    req.headers.scheme = "http";
+    var options = {
+        host: req.host,
+        port: 80,
+        path: "/",
+        headers: req.headers
+    };
+    var proxyReq = http.request(options, function (res) {
+        if (res == null) {
+            throw new Error("nima");
         }
-        req.headers.scheme = "http";
-        var options = {
-            host: req.host,
-            port: 80,
-            path: "/",
-            headers: req.headers
-        };
-        var proxyReq = http.request(options, function (res) {
-            if (res == null) {
-                throw new Error("nima");
-            }
-            var parsedHeader = {}
+        var parsedHeader = {}
 
-            for (var name in res.headers) {
-                if (!(name in {
-                        'connection': "",
-                        'host': "",
-                        'keep-alive': "",
-                        'proxy-connection': "",
-                        'transfer-encoding': "",
-                        'upgrade': ""
-                    })) {
-                    parsedHeader['' + name] = res.headers['' + name]
-                }
+        for (var name in res.headers) {
+            if (!(name in {
+                    'connection': "",
+                    'host': "",
+                    'keep-alive': "",
+                    'proxy-connection': "",
+                    'transfer-encoding': "",
+                    'upgrade': ""
+                })) {
+                parsedHeader['' + name] = res.headers['' + name]
             }
-            resp.writeHead(res.statusCode, parsedHeader);
-            res.on('data', function (chunk) {
-                resp.write(chunk);
-            });
-            res.on('end', function () {
-                resp.end();
-            });
-
-            console.log(req.url);
+        }
+        resp.writeHead(res.statusCode, parsedHeader);
+        res.on('data', function (chunk) {
+            resp.write(chunk);
         });
-        req.pipe(proxyReq);
-        proxyReq.end();
-    }
+        res.on('end', function () {
+            resp.end();
+        });
 
-// function (request, response) {
-//     var queryObject = url.parse(request.url, true);
-//     if (!queryObject) {
-//         response.writeHead(404, "not found");
-//         response.end();
-//     }
-//     else {
-//         var proxyUrl = url.parse(queryObject);
-//         proxyUrl.method = request.method;
-//         proxyUrl.headers = request.headers;
-//         proxyUrl.headers["accept-encoding"] = "identity";
-//         proxyUrl.headers["referer"] = "";
-//         proxyUrl.headers["host"] = url.hostname;
-//         proxyUrl.host = request.host;
-//
-//         var proxy_request;
-//
-//         proxy_request = http.request(proxyUrl);
-//
-//
-//         proxy_request.on("response", function (proxy_response) {
-//
-//             response.writeHead(200, proxy_response.headers);
-//             console.log("content-type " + proxy_response.headers["content-type"]);
-//             if (proxy_response.headers && proxy_response.headers["content-type"] && proxy_response.headers["content-type"].indexOf("text") != -1 && proxy_response.headers["content-type"].indexOf("html") != -1) {
-//                 console.log("Turning on html parser.");
-//                 var htmlparser = require("htmlparser2");
-//                 var handler = require("./nodeHandler");
-//                 var Parser = new htmlparser.Parser(handler.handler);
-//                 handler.setContext(proxyUrl);
-//                 proxy_response.on('data', function (chunk) {
-//                     Parser.write(chunk);
-//                 });
-//                 proxy_response.on('end', function () {
-//                     response.write("" + handler.getHTML());
-//                     response.end();
-//                     Parser.end()
-//                     Parser.reset();
-//                 });
-//
-//
-//             }
-//             else {
-//                 console.log("writing binary data");
-//                 proxy_response.on('data', function (chunk) {
-//                     response.write(chunk, "binary");
-//                 });
-//                 proxy_response.on('end', function () {
-//                     response.end();
-//                 });
-//             }
-//
-//
-//         });
-//
-//
-//         console.log(proxyUrl);
-//
-//         request.on('data', function (chunk) {
-//             proxy_request.write(chunk, 'binary');
-//         });
-//         request.on('end', function () {
-//             proxy_request.end();
-//         });
-//     }
-// }
-).listen(8443, function (err) {
+        console.log(req.url);
+    });
+    req.pipe(proxyReq);
+    proxyReq.end();
+}).on('connect', function (req, socket) {
+    var requestOptions = {
+        host: req.headers.host.split(':')[0],
+        port: req.headers.host.split(':')[1] || 443,
+    };
+    // if (options.localAddress) {
+    //     requestOptions.localAddress = options.localAddress;
+    // }
+
+    var tunnel = net.createConnection(requestOptions, function () {
+        // socket.writeHead(200,"Connection established",{});
+        synReply(socket, 200, 'Connection established',
+            {
+                'Connection': 'keep-alive',
+                'Proxy-Agent': 'SPDY Proxy '
+            },
+            function () {
+                tunnel.pipe(socket);
+                socket.pipe(tunnel);
+            }
+        );
+    });
+
+    tunnel.setNoDelay(true);
+
+    tunnel.on('error', function (e) {
+        console.log("Tunnel error: ".red + e);
+        synReply(socket, 502, "Tunnel Error", {}, function () {
+            socket.end();
+        });
+    });
+}).listen(8443, function (err) {
     console.log(err);
 });
 
+function synReply(socket, code, reason, headers, cb) {
+    try {
+        if (socket._handle) {
+            socket._handle._spdyState.stream._spdyState.framer.headersFrame(
+                {
+                    status: 200
+                }
+            );
+
+
+            // Chrome used raw SSL instead of SPDY when issuing CONNECT for
+            // WebSockets. Hence, to support WS we must fallback to regular
+            // HTTPS tunelling: https://github.com/igrigorik/node-spdyproxy/issues/26
+        } else {
+
+            var statusLine = 'HTTP/1.1 ' + code + ' ' + reason + '\r\n';
+            var headerLines = '';
+            for (var key in headers) {
+                headerLines += key + ': ' + headers[key] + '\r\n';
+            }
+            socket.write(statusLine + headerLines + '\r\n', 'UTF-8', cb);
+        }
+    } catch (error) {
+        console.log(error);
+        cb.call();
+    }
+}
 module.exports = app;
