93d9d9c4b5feeec70e1b5f31f4e4afab1f30e4fa
peterae86 <peter26983045>
2016-08-08 00:33:01 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('header', { title: '施工中' });
+router.get('/', function (req, res, next) {
+    var stream = res.push('/javascript/header.js', {
+        status: 200, // optional
+        method: 'GET', // optional
+        request: {
+            accept: '*/*'
+        },
+        response: {
+            'content-type': 'application/javascript'
+        }
+    });
+    stream.on('error', function (err) {
+        console.log(err);
+    });
+    stream.end(fs.readFileSync('web/public/javascript/header.js'));
+    res.render('header', {title: '施工中'});
 });
 
 
