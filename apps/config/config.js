6e42c01105a1e40c6d8d6dd24f7964d6b0a96139
xiaorui.guo <775870132@qq.com>
2017-08-08 22:30:18 +0800

    key

--- apps/config/config.js
+++ apps/config/config.js
@@ -2,8 +2,8 @@ var fs = require("fs");
 var argv = require('minimist')(process.argv.slice(2));
 module.exports = {
     httpsConfig: {
-        key: fs.readFileSync('./my.key'),
-        cert: fs.readFileSync('./my.crt')
+        key: fs.readFileSync('/root/backkoms.com.key'),
+        cert: fs.readFileSync('/root/backkoms.crt')
     },
     argv: argv
 };
