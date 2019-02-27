5182837e8c5779b94206ac07a82e840e874ccd16
guoxiaorui <guoxiaorui@xiaomi.com>
2019-02-27 20:58:53 +0800

    fix

--- apps/proxy.js
+++ apps/proxy.js
@@ -1,6 +1,8 @@
 var config = require("./config/config");
 var proxy = require('./proxy/proxy');
+var proxy2 = require('./proxy/httpProxy');
 function run() {
     proxy(config.httpsConfig, 8443);
+    new proxy2({}).start()
 }
 module.exports = run;
