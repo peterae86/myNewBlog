0d3712bc2856c2f8aff669a48f5511697569a8d8
peterae86 <775870132@qq.com>
2019-11-19 07:54:00 +0800

    Update blog.js

--- apps/blog.js
+++ apps/blog.js
@@ -2,6 +2,6 @@ var http2 = require('spdy');
 var blog = require('./blog/blog');
 var config = require("./config/config");
 function run() {
-    http2.createServer(config.httpsConfig, blog).listen(443);
+    http2.createServer(config.httpsConfig, blog).listen(4567);
 }
 module.exports = run;
